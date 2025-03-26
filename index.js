c3dd0292e2abb6951ccbb6ccbb4b7f49739231a1
POHMONSEY22 <juanarboledag610@gmail.com>
2025-03-26 09:14:56 -0500

    Licoreralosj v1.0.5 creacion del index SebastianSanchez

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const exp = require('express');
+require('dotenv').config();
+const enrutador = require('/routes/router');
+
+const app = exp();
+
+app.use(exp.json());
+app.use(exp.urlencoded({extended:false}));
+app.use('/Licoreraj',enrutador);
+
+
+app.listen(process.env.PORT, () => {
+    console.log("estamos en elpuerto" + process.env.PORT);
+})
 
